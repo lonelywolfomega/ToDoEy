166882659855f390b372ff6455c54034a224acc9
Okhunov Odil <odiljon.ahunov.1997@mail.ru>
2020-12-04 22:38:06 +0900

    Final commit

---
+++ ToDoEy/Category.swift
@@ -0,0 +1,8 @@
+//
+//  Category.swift
+//  ToDoEy
+//
+//  Created by Okhunov Odil on 2020/12/04.
+//
+
+import Foundation
---
+++ ToDoEy/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  ToDoEy
+//
+//  Created by Okhunov Odil on 2020/12/04.
+//
+
+import Foundation
